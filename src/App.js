2585e342ce56b7692aef4f464b62c7a34b53db71
Sylvadoc <legacjf@gmail.com>
2017-07-25 11:48:27 +0200

    test appel json wordpress depuis un front en js et en react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			livres: []
+		}
+	}
+	componentDidMount() {
+		let dataURL = "http://www.elbakin.net/taniquetil/wp-json/wp/v2/livre";
+		fetch(dataURL)
+			.then(res => res.json())
+			.then(res => {
+				this.setState({
+					livres: res
+				})
+			})
+	}
+	render() {
+		let livres = this.state.livres.map((livre, index) => {
+			return <li key={index}>
+				<h2>{livre.title.rendered}</h2>
+				<ul>
+					<li>Extrait : <div dangerouslySetInnerHTML={ {__html: livre.excerpt.rendered} } /></li>
+					<li>Format : {livre.acf.format}</li>
+					<li>&Eacute;diteur : {livre.acf.editeur}</li>
+					<li>Collection : {livre.acf.collection}</li>
+					<li>Illustration : {livre.acf.illustration}</li>
+					<li>Traduction : {livre.acf.traduction}</li>
+					<li>ISBN : {livre.acf.isbn_13}</li>
+					<li>Note : {livre.acf.note} / 10</li>
+				</ul>
+			</li>
+		});
+		return (
+			<div>
+				<h1>react from a wordpress backoffice</h1>
+				<ul>
+					{livres}
+				</ul>
+			</div>
+		)
+	}
 }
-
 export default App;
