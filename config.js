ec48604cadbd3bcb3eab22434a33da9d43c185c6
Sylvadoc <legacjf@gmail.com>
2017-07-26 16:36:22 +0200

    dev dependencies

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	// -- URL de l'API REST Wordpress --
+	restBackendUrl: 'http://www.elbakin.net/taniquetil/wp-json/wp/v2/', // Prod
+
+	// -- ID GOOGLE --
+	googleSiteVerification: 'Uqyo9RBa281bNahtsN76roLqWavGWr28eXHyJEtdi4U',
+
+};
