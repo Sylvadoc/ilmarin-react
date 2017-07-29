385199f6b12cb6db7fe6f1c0b80f173ae1873345
Sylvadoc <legacjf@gmail.com>
2017-07-29 09:25:17 +0200

    configuration npm & gulp

---
+++ gulpfile.js
@@ -0,0 +1,143 @@
+'use strict';
+
+// Load plugins
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+	imageminSvgo = require('imagemin-svgo'),
+	pngquant = require('imagemin-pngquant'),
+	svgSprite = require('gulp-svg-sprite'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    sourcemaps = require('gulp-sourcemaps'),
+    del = require('del');
+
+// configuration des chemins
+var SASS_FILES_DIR = './styles';
+var SASS_FILES_PATH = SASS_FILES_DIR + '/**/*.scss';
+var BUNDLED_CSS_FILES_PATH = './public/css';
+var ASSETS_FILES_PATH = './assets/**/*';
+var BUNDLED_ASSETS_FILES_PATH = './public';
+var ASSETS_IMGS_PATH = './assets/img/**/*';
+var ASSETS_SVGS_ICONS_PATH = './assets/svg/icons/**/*.svg';
+var OPT_ASSETS_IMGS_PATH = './public/img';
+var OPT_ASSETS_SVGS_ICONS_PATH = './public/svg/icons/';
+
+// configuration des plugins
+var SASS_COMPILE_OPTIONS = {
+    outputStyle: 'expanded'
+};
+var autoprefixerOptions = {
+	browsers: ['last 2 versions', 'Explorer >= 11']
+};
+var imageminConfig = {
+	progressive: true,
+	use: [pngquant()],
+	svgoPlugins: [
+		{
+			removeViewBox: false
+		}, {
+			cleanupIDs: false
+		}
+	]
+};
+var svgConfig = {
+	shape               : {
+		dimension       : {         // Set maximum dimensions
+			maxWidth    : 32,
+			maxHeight   : 32
+		},
+		spacing         : {         // Add padding
+			padding     : 0
+		}
+	},
+	mode                : {
+		view            : {         // Activate the «view» mode
+			bust        : false,
+			render      : {
+				scss    : true      // Activate Sass output (with default options)
+			}
+		},
+		symbol          : true      // Activate the «symbol» mode
+	}
+};
+
+// copy static files
+gulp.task('assets', function () {
+    return gulp.src(ASSETS_FILES_PATH)
+        .pipe(gulp.dest(BUNDLED_ASSETS_FILES_PATH))
+});
+
+// Styles
+gulp.task('styles', function() {
+	return gulp.src(SASS_FILES_PATH)
+		.pipe(sass(SASS_COMPILE_OPTIONS).on('error', sass.logError))
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer(autoprefixerOptions))
+		.pipe(gulp.dest(BUNDLED_CSS_FILES_PATH))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(cssnano({
+			safe: true,
+			postcssZindex: false,
+			zindex: false,
+            autoprefixer: { autoprefixerOptions }
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(BUNDLED_CSS_FILES_PATH))
+});
+
+// Scripts
+/*gulp.task('scripts', function() {
+  return gulp.src('scripts/src/!**!/!*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});*/
+
+// Images
+gulp.task('images', function() {
+	return gulp.src(ASSETS_IMGS_PATH)
+		.pipe(imagemin(imageminConfig))
+		.pipe(gulp.dest(OPT_ASSETS_IMGS_PATH))
+});
+
+// About svgs
+gulp.task('build_svg', function () {
+	return gulp.src(ASSETS_SVGS_ICONS_PATH)
+		.pipe(svgSprite(svgConfig))
+		.pipe(gulp.dest(OPT_ASSETS_SVGS_ICONS_PATH));
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+  gulp.start('assets','styles', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch(SASS_FILES_PATH, ['styles']);
+
+  // Watch image files
+  gulp.watch(ASSETS_IMGS_PATH, ['images']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch([BUNDLED_ASSETS_FILES_PATH]).on('change', livereload.changed);
+
+});
