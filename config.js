ffb22d9215714599a554c308f958243e6e46d6b0
Sylvadoc <legacjf@gmail.com>
2017-07-29 15:13:39 +0200

    nomenclature

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 module.exports = {
 
+	// -- port d'écoute de la webapp
+    port: 3000,
+
 	// -- URL de l'API REST Wordpress --
 	restBackendUrl: 'http://www.elbakin.net/taniquetil/wp-json/wp/v2/', // Prod
 
 	// -- ID GOOGLE --
-	googleSiteVerification: 'Uqyo9RBa281bNahtsN76roLqWavGWr28eXHyJEtdi4U',
+	googleSiteVerification: 'Uqyo9RBa281bNahtsN76roLqWavGWr28eXHyJEtdi4U'
 
 };
