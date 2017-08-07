378d9d434a30ba2a2ad3353da5fce6fc8a91b2c1
Sylvadoc <legacjf@gmail.com>
2017-08-07 14:37:19 +0200

    non fonctionnel - création des routes et d'une première page bibliotheque

--- src/index.app.js
+++ src/index.app.js
@@ -12,16 +12,6 @@ import { bindActionCreators } from "redux";
 
 class IndexApp extends Component {
 
-	componentWillMount() {
-		// On communique par le store, c'est pourquoi on récupère les params pour le setter dans le store
-		this.props.setRouterParams(this.props.params, this.props.location.pathname + this.props.location.search);
-	}
-
-	componentWillUpdate(nextProps) {
-		// On communique par le store, c'est pourquoi on récupère les params pour le setter dans le store
-		this.props.setRouterParams(nextProps.params, nextProps.location.pathname + nextProps.location.search);
-	}
-
 	render() {
 		return this.props.children;
 	}
