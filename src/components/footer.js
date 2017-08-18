9806160dca4b3cbe545ba692dc8410321bb2ba52
Sylvadoc <legacjf@gmail.com>
2017-08-18 14:42:05 +0200

    [JF] ~ React ~ footer connecté

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,7 +13,8 @@ export class Footer extends Component {
     constructor() {
         super();
         this.state = {
-            pages: [],
+            pagesContact: [],
+            pagesLeSite: [],
             weather: ['sunny','cloudy','rainy','snowy','rainbow','starry','stormy'],
             cities: ['Minas Tirith','Fendeval','Hobbiteville','Minas Morgul','Camorr','Ankh-Morpork','Tar Valon','Port-Réal','Castelcerf','Poudlard','Wielstadt','Edoras','Luthadel','Lorgol','Dros Delnoch','Ymrrir','Sunnydale','Konoha','Benden','Riva']
         }
@@ -22,11 +23,20 @@ export class Footer extends Component {
     componentDidMount() {
         // appel du json de wordpress consacré aux pages
         let dataURL = "http://www.elbakin.net/taniquetil/wp-json/wp/v2/pages";
-        fetch(dataURL)
+        // seulement les pages dont le parent est Contactez-nous
+        fetch(dataURL + "?parent=494")
             .then(res => res.json())
             .then(res => {
                 this.setState({
-                    pages: res
+                    pagesContact: res
+                })
+            });
+        // seulement les pages dont le parent est Le Site
+        fetch(dataURL + "?parent=492")
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    pagesLeSite: res
                 })
             });
     }
@@ -37,8 +47,17 @@ export class Footer extends Component {
         const weather = this.state.weather[random(this.state.weather.length)];
         const cities = this.state.cities[random(this.state.cities.length)];
 
-        // construction de la liste de pages
-        let pagesList = this.state.pages.map((page, index) => {
+        // construction de la liste des pages contactez-nous
+        let pagesContactList = this.state.pagesContact.map((page, index) => {
+            return (
+                <li key={"page-" +index}>
+                    <Link to={"/" + page.id + "/" + page.slug}>{page.title.rendered}</Link>
+                </li>
+            )
+        });
+
+        // construction de la liste des pages Le Site
+        let pagesSiteList = this.state.pagesLeSite.map((page, index) => {
             return (
                 <li key={"page-" +index}>
                     <Link to={"/" + page.id + "/" + page.slug}>{page.title.rendered}</Link>
@@ -55,7 +74,7 @@ export class Footer extends Component {
                         <strong id="weather_city_text">{cities}</strong>
                     </span>
 						<div id="content_icon">
-							<div id="weather" className={weather}></div>
+							<div id="weather" className={weather}>&nbsp;</div>
 						</div>
 					</div>
 					<div className="small-6 medium-3 large-2 columns">
@@ -65,23 +84,18 @@ export class Footer extends Component {
 							<li><a href="http://archibald.elbakin.net" target="_blank"  rel="noopener noreferrer" title="Feuilleton numérique des Fabuleuses Aventures d'Archibald Bellérophon (nouvelle fenêtre)">Archibald Bellérophon</a></li>
 						</ul>
 					</div>
-					<div className="small-6 medium-6 large-2 columns">
-						<h2>Le site</h2>
-						<ul className="basic_list">
-							<li><Link to="/">Accueil</Link></li>
-                            {pagesList}
-						</ul>
-					</div>
-					<div className="small-12 medium-6 large-3 columns">
-						<h2>Contactez-nous</h2>
-						<ul className="basic_list">
-							<li><a href="/">Une coquille, une erreur ?</a></li>
-							<li><a href="/">L'équipe du site</a></li>
-							<li><a href="/">&Agrave; propos de la ligne éditoriale</a></li>
-							<li><a href="/">&Agrave; propos de la publicité/partenariats</a></li>
-							<li><a href="/">Pour participer à la vie du site</a></li>
-						</ul>
-					</div>
+                    <div className="small-6 medium-6 large-2 columns">
+                        <h2>Le Site</h2>
+                        <ul className="basic_list">
+                            {pagesSiteList}
+                        </ul>
+                    </div>
+                    <div className="small-6 medium-6 large-3 columns">
+                        <h2>Contactez-nous</h2>
+                        <ul className="basic_list">
+                            {pagesContactList}
+                        </ul>
+                    </div>
 					<div className="small-12 medium-6 large-3 columns">
 						<h2>Restons en contact !</h2>
 						<form className="newsletterform" method="get" action="/">
@@ -91,9 +105,9 @@ export class Footer extends Component {
 						</form>
 						<p>Et rejoignez-nous sur les réseaux sociaux :</p>
 						<ul className="social_footer">
-							<li><a href="http://twitter.com/elbakin" target="_blank" rel="noopener noreferrer"><span className="icon icon-twitter" aria-hidden="true"></span><span className="text">Twitter</span></a></li>
+							<li><a href="https://twitter.com/elbakin" target="_blank" rel="noopener noreferrer"><span className="icon icon-twitter" aria-hidden="true"></span><span className="text">Twitter</span></a></li>
 							<li><a href="https://www.facebook.com/Elbakin.net" target="_blank" rel="noopener noreferrer"><span className="icon icon-facebook" aria-hidden="true"></span><span className="text">Facebook</span></a></li>
-							<li><a href="https://plus.google.com/116698928900965945603" target="_blank" rel="noopener noreferrer"><span className="icon icon-google-plus" aria-hidden="true"></span><span className="text">Google Plus</span></a></li>
+							<li><a href="https://plus.google.com/+ElbakinNetFantasy" target="_blank" rel="noopener noreferrer"><span className="icon icon-google-plus" aria-hidden="true"></span><span className="text">Google Plus</span></a></li>
 						</ul>
 					</div>
 				</div>
