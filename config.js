29404523e83f91e18597f325792c5b6eddacf3eb
Sylvadoc <legacjf@gmail.com>
2019-12-10 20:46:36 +0100

    fix url https

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     port: 3000,
 
 	// -- URL de l'API REST Wordpress --
-	restBackendUrl: 'http://www.elbakin.net/taniquetil/wp-json/wp/v2/', // Prod
+	restBackendUrl: 'https://www.taniquetil.site/taniquetil/wp-json/wp/v2/', // Prod
 
 	// -- ID GOOGLE --
 	googleSiteVerification: 'Uqyo9RBa281bNahtsN76roLqWavGWr28eXHyJEtdi4U'
